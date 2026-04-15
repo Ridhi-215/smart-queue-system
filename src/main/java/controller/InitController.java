95a049786a26c5aab72ab09c226eeffd2de1e584
Ridhi-215 <gunturridhi@gmail.com>
2026-04-15 20:12:55 +0530

    Remove ServiceCenter dependency

--- src/main/java/controller/InitController.java
+++ src/main/java/controller/InitController.java
@@ -1,12 +1,9 @@
 package com.smartqueue.controller;
 
-import com.smartqueue.model.ServiceCenter;
 import com.smartqueue.model.Queue;
 import com.smartqueue.model.User;
-import com.smartqueue.repository.ServiceCenterRepository;
 import com.smartqueue.repository.QueueRepository;
 import com.smartqueue.repository.UserRepository;
-import org.springframework.stereotype.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -14,9 +11,6 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/init")
 public class InitController {
-
-    @Autowired
-    private ServiceCenterRepository serviceCenterRepository;
 
     @Autowired
     private QueueRepository queueRepository;
@@ -27,16 +21,9 @@ public class InitController {
     @GetMapping("/setup")
     public String setup() {
 
-        ServiceCenter sc = new ServiceCenter();
-        sc.setName("HDFC Bank");
-        sc.setLocation("Mumbai");
-        sc.setType("BANK");
-        serviceCenterRepository.save(sc);
-
         Queue q = new Queue();
-        q.setName("Withdrawal");
+        q.setName("Test Queue");
         q.setStatus("OPEN");
-        q.setServiceCenter(sc);
         queueRepository.save(q);
 
         User u = new User();
